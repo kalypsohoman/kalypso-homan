5568c021ca79ae77b4d0bba07fde8423cddd1fb6
kalypsohoman <kalypsohoman@gmail.com>
2024-03-08 19:11:45 -0600

    Change the starting text from default sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to MY WEBSITE</h1>
+<p>Things are still in the works but feel free to click around</p>
