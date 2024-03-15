ea545cfd0789c27bc5130456eba9d35c0aec432a
kalypsohoman <kalypsohoman@gmail.com>
2024-03-15 10:34:20 -0500

    Init

---
+++ src/lib/CodeProjects.ts
@@ -0,0 +1,47 @@
+export let codeProjects = [
+    {
+        name: 'ManHunt',
+        image: 'images/projects/Manhunt.jpeg',
+        alt: 'ManHunt logo',
+        description: 'An AR bounty hunting game where users would race to capture suspects using facial recognition on their phones',
+        techStack: ['TypeScript', 'React', 'Node', 'Socket.IO', 'Tensorflow', '3js', 'AWS', 'PostgresSQL', 'Sequelize'],
+        gitLink: '',
+        link: null,
+    },
+    {
+        name: 'Delightful Little Treat',
+        image: 'images/projects/Manhunt.jpeg',
+        alt: 'ManHunt logo',
+        description: 'An AR bounty hunting game where users would race to capture suspects using facial recognition on their phones',
+        techStack: ['TypeScript', 'React', 'Node', 'Socket.IO', 'Tensorflow', '3js', 'AWS', 'PostgresSQL', 'Sequelize'],
+        gitLink: '',
+        link: 'delightfullittletreat.com',
+    },
+    {
+        name: 'JSF*ck',
+        image: 'images/projects/Manhunt.jpeg',
+        alt: 'ManHunt logo',
+        description: 'An AR bounty hunting game where users would race to capture suspects using facial recognition on their phones',
+        techStack: ['TypeScript', 'React', 'Node', 'Socket.IO', 'Tensorflow', '3js', 'AWS', 'PostgresSQL', 'Sequelize'],
+        gitLink: '',
+        link: null,
+    },
+    {
+        name: '',
+        image: 'images/projects/Manhunt.jpeg',
+        alt: 'ManHunt logo',
+        description: 'An AR bounty hunting game where users would race to capture suspects using facial recognition on their phones',
+        techStack: ['TypeScript', 'React', 'Node', 'Socket.IO', 'Tensorflow', '3js', 'AWS', 'PostgresSQL', 'Sequelize'],
+        gitLink: '',
+        link: null,
+    },
+    {
+        name: 'Fellowship Finder',
+        image: 'images/projects/Manhunt.jpeg',
+        alt: 'ManHunt logo',
+        description: 'An AR bounty hunting game where users would race to capture suspects using facial recognition on their phones',
+        techStack: ['TypeScript', 'React', 'Node', 'Socket.IO', 'Tensorflow', '3js', 'AWS', 'PostgresSQL', 'Sequelize'],
+        gitLink: '',
+        link: null,
+    }
+]
