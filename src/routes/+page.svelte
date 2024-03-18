5c9917fb9d336c0db8a3022a0e957a9875ec1655
kalypsohoman <kalypsohoman@gmail.com>
2024-03-18 16:46:49 -0500

    Add container for projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,48 @@
     import { codeProjects, Project } from "$lib";
 </script>
 
-<img class='logo' src='images/kalypso.png' alt='kalypso'>
-{#each codeProjects as codeProject}
-    <Project options={codeProject}/>
-{/each}
+<img class='logo' src='images/PROJECTS.png' alt='PROJECTS'>
+<div class='projects'>
+    {#each codeProjects as codeProject}
+        <Project options={codeProject}/>
+    {/each}
+</div>
 
 <style lang=scss>
+    .logo {
+        width: min(80%, 70vh);
+        margin: 17vh auto
+    }
 
-.logo {
-    width: min(80%, 70vh);
-    margin: 150px auto
+    .projects {
+        display: flex;
+        flex-direction: row;
+        overflow-x: auto;
+        -webkit-overflow-scrolling: touch;
+        gap: 20px;
+        padding: 20px;
+        box-sizing: border-box;
+        max-height: 100vh;
+        width: 90%;
+
+        scrollbar-width: thin; /* For Firefox */
+    scrollbar-color: darkgray lightgray; /* For Firefox, scrollbar thumb and track */
+
+    /* Scrollbar track (background) */
+    .contents::-webkit-scrollbar-track {
+        background: white; /* Sets the track background to white */
+        border: 1px solid white; /* Optional: Adds a border to the scrollbar track */
+    }
+
+    /* Scrollbar thumb (the scrollable element that you interact with) */
+    .contents::-webkit-scrollbar-thumb {
+        background: orange; /* Sets the scrollbar thumb to orange */
+    }
+
+    /* Scrollbar width and height for vertical scrollbars */
+    .contents::-webkit-scrollbar {
+        width: 10px; /* Width of the vertical scrollbar */
+    }
     }
 </style>
     
