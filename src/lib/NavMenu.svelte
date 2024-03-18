44f010f9bd19a706515bf2fdc83271f2fbbc1f82
kalypsohoman <kalypsohoman@gmail.com>
2024-03-18 12:21:35 -0500

    Remove art from nav links

--- src/lib/NavMenu.svelte
+++ src/lib/NavMenu.svelte
@@ -8,22 +8,24 @@
     page.subscribe(page => {currentPage = page.route.id})
     let open:boolean = false;
 
-    export const links = ['art', 'coding', 'about'];
 </script>
 
 <Hamburger bind:open/>
 
 {#if open}
     <div class='hamburger-menu-contents' transition:fly={{ y: -15}}>
-        {#each links as link, i}
-            <a class={currentPage === '/' + link ? 'current-page' : ''}
-                href={'/' + link}
-                on:click={()=> open = !open}
-                transition:fly={{ y: -15, delay: 50 }}>
-                    {link.toUpperCase()}
-                
-            </a>
-        {/each}
+        <a class={currentPage === '/' ? 'current-page' : ''}
+            href={'/'}
+            on:click={()=> open = !open}
+            transition:fly={{ y: -15, delay: 50 }}>
+            CODING
+        </a>
+        <a class={currentPage === '/about' ? 'current-page' : ''}
+            href={'/about'}
+            on:click={()=> open = !open}
+            transition:fly={{ y: -15, delay: 50 }}>
+            ABOUT
+        </a>
     </div>
 {/if}
 
@@ -32,24 +34,23 @@
         text-align: center;
         font-size: 1.5em;
         letter-spacing: 0.15em;
-        padding: 1em;
         position: absolute;
+        padding: 20px 0;
         top: 80px;
-        left: 0;
         width: 100vw;
         background-color: rgb(0 0 0);
         z-index: 1000;
         border: white solid 1px;
         border-inline: none;
-        display: none;
         flex-direction: column;
+        gap: 1rem;
     }
 
     a {
         font-family: 'Mourgen';
         cursor: pointer;
         width: max-content;
-        margin: 1rem auto;
+        margin: 10px auto;
         text-decoration: none;
         color: white;
     }
