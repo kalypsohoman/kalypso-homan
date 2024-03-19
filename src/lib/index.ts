369ec76b171f49ec5261581dd2c35aa2028c1df4
kalypsohoman <kalypsohoman@gmail.com>
2024-03-19 12:14:35 -0500

    Add CloseButton and SocialMediaLinks. Alphabetize.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,8 @@
-export {default as Contact} from "./Contact.svelte";
 export {default as Bio} from "./Bio.svelte";
-export {default as Resume} from "./Resume.svelte";
+export {default as CloseButton} from "./CloseButton.svelte";
+export {default as Contact} from "./Contact.svelte";
 export {default as NavMenu} from "./NavMenu.svelte";
 export {default as Project} from "./Project.svelte";
+export {default as Resume} from "./Resume.svelte";
+export {default as SocialMediaLinks} from "./SocialMediaLinks.svelte";
 export { codeProjects } from "./codeProjects";
