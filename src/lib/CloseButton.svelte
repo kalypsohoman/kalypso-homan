a86d144e2f104cb855f6ff211630a9da9ff3f2e6
kalypsohoman <kalypsohoman@gmail.com>
2024-03-19 12:12:58 -0500

    Init

---
+++ src/lib/CloseButton.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+    import { quadInOut } from "svelte/easing";
+    import { createEventDispatcher } from 'svelte';
+    export let visible: boolean = true;
+
+    const dispatch = createEventDispatcher();
+
+    function handleClick() {
+        visible = false;
+        dispatch('click');
+    }
+
+    function spin(node: HTMLSpanElement, {duration, rotate}: {duration: number, rotate: number}) {
+		return {
+			duration,
+			css: (t: number) => {
+				const eased = quadInOut(t);
+				return `
+					transform:  rotate(${eased * rotate}deg);
+					`;
+			}
+		};
+	}
+</script>
+
+{#if visible}
+    <button
+        class='close'
+        aria-label={'close'}
+        on:click={handleClick}
+        >
+        <span class="close-box">
+            <span class="close-inner first-line" out:spin={{ duration: 500, rotate: 125 }}/>
+            <span class="close-inner second-line" out:spin={{ duration: 500, rotate: 205 }}/>
+        </span>
+    </button>
+{/if}
+
+<style lang="scss">
+    .close{
+        display: contents;
+        &:hover {
+            color: orange;
+        }
+    }
+
+    .close-box {
+        border: solid white 2px;
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+        &:hover {
+            border: solid orange 2px;
+            cursor: pointer;
+            .first-line, .second-line {  
+                background-color: orange;
+            }
+        }
+    }
+
+    .close-inner {
+        display: block;
+        width: 20px;
+        height: 1px;
+        background-color: white;
+        position: relative;
+        &.first-line {
+            transform: rotate(45deg);
+            left: 5px;
+            top: 14px;
+
+        }
+        &.second-line {
+            transform: rotate(-45deg);
+            left: 5px;
+            top: 13px;
+        }
+    }
+</style>
+
---
+++ src/lib/SocialMediaLinks.svelte
@@ -0,0 +1,28 @@
+<div class='social-media-links'>
+    <a class='social-media-link github' href="https://github.com/kalypsohoman" target="_blank">
+        <img src="images/icons/github.png" alt="Link to my Github">
+    </a>
+    <a class='social-media-link linkedin' href="www.linkedin.com/in/kalypsohoman/" target="_blank">
+        <img src="images/icons/linkedin.png" alt="Link to my LinkedIn">
+    </a>
+    <a class='social-media-link instagram' href="https://www.instagram.com/kalypso_designs/" target="_blank">
+        <img src="images/icons/instagram.png" alt="Link to my Instagram">
+    </a>
+</div>
+
+<style lang="scss">
+    .social-media-links {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        & img {
+            display: block;
+            max-width: 50%;
+            height: auto;
+            border: 0;
+            margin-top: 20px;
+            margin-inline: auto;
+        }
+    }
+</style>
