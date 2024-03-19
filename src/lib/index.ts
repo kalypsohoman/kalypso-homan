3a254e2bcc827c4c3ccff4b906a7754f8841a90b
kalypsohoman <kalypsohoman@gmail.com>
2024-03-19 14:29:51 -0500

    Remove Resume export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,5 @@ export {default as CloseButton} from "./CloseButton.svelte";
 export {default as Contact} from "./Contact.svelte";
 export {default as NavMenu} from "./NavMenu.svelte";
 export {default as Project} from "./Project.svelte";
-export {default as Resume} from "./Resume.svelte";
 export {default as SocialMediaLinks} from "./SocialMediaLinks.svelte";
 export { codeProjects } from "./codeProjects";
