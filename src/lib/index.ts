b5fd1fce85f37d56bb8f3facdd8c4f49326ad530
kalypsohoman <kalypsohoman@gmail.com>
2024-03-21 10:56:26 -0500

    Export PopupContainer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ export {default as Bio} from "./Bio.svelte";
 export {default as CloseButton} from "./CloseButton.svelte";
 export {default as Contact} from "./Contact.svelte";
 export {default as NavMenu} from "./NavMenu.svelte";
+export {default as PopupContainer} from "./PopupContainer.svelte";
 export {default as Project} from "./Project.svelte";
 export {default as SocialMediaLinks} from "./SocialMediaLinks.svelte";
 export { codeProjects } from "./codeProjects";
