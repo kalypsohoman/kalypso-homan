27f5840d009f1d254ffba87f14a7cdc211862143
kalypsohoman <kalypsohoman@gmail.com>
2024-03-21 10:02:22 -0500

    Add resume sub tabs with transition animations. Change hover to orange color and active to orange border.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,10 @@
 <!-- TODO: make bio only clickable from ghost :oooo -->
 
+
 <script lang=ts>
     import {Contact, Bio, CloseButton, SocialMediaLinks} from '$lib';
+	import { quadInOut } from 'svelte/easing';
+    import { slide } from 'svelte/transition';
     export let activeTab: string = 'Contact';
 
     let resumeOpen = false;
@@ -17,18 +20,27 @@
     <div class='col-2'>
 
         <div class='tab-container'>
-            <div class='tab' class:active={activeTab === 'Contact'}>
-                <button on:click={() => activeTab = 'Contact'}>Contact</button>
-            </div>
-            <div class='tab' class:active={activeTab === 'Resume'}>
-                <button on:click={() => activeTab = 'Resume'}>Resume</button>
-            </div>
-            {#if activeTab==='Resume'}
-                <button class='sub-tab' on:click={() => resumeOpen = true}>Open Larger</button>
                 <button
-                    class='sub-tab'
-                    on:click={() => window.open('kalypso-homan-resume.jpg', '_blank')}>
-                    Open in New Tab
+                    class='tab'
+                    class:active={activeTab === 'Contact'}
+                    on:click={() => activeTab = 'Contact'}>Contact
+                </button>
+                <button
+                    class='tab resume-tab'
+                    class:active={activeTab === 'Resume'}
+                    on:click={() => activeTab = 'Resume'}>Resume
+            </button>
+
+            {#if activeTab==='Resume'}
+                <button
+                    class='resume-sub-tab tab'
+                    on:click={() => resumeOpen = true}
+                    transition:slide={{ delay: 0, duration: 300, easing: quadInOut, axis: 'x' }}>Open Larger
+                </button>
+                <button
+                    class='resume-sub-tab tab'
+                    on:click={() => window.open('kalypso-homan-resume.jpg', '_blank')}
+                    transition:slide={{ delay: 0, duration: 600, easing: quadInOut, axis: 'x' }}>Open in New Tab
                 </button>
             {/if}
         </div>
@@ -70,8 +82,6 @@
      .headshot{
         max-width: 80%;
     }
-
-
     .tab-container {
         display: flex;
         flex-wrap: wrap;
@@ -79,30 +89,31 @@
     }
     .tab {
         width: 20%;
+        height: 50px;
+        align-items: center;
         cursor: pointer;
-        border: 1px solid white;
+        box-shadow: inset 0 0 0 1px white;
         margin: 2%;
         padding: 0;
         display: flex;
         justify-content: center;
-    }
-
-    .tab:hover {
-        border: solid 1px orange;
-    }
-    .tab.active {
-        box-shadow: inset 0 0 0 2px orange;
-        border: solid orange 1px;
-    }
-    .tab button {
         background-color: transparent;
         border: none;
         color: white;
-        font-family: 'Enigma Bold';
-        height: 100%;
-        padding: 2vw;
-        cursor: pointer;
+        font-family: 'Caviar Dreams';
+        font-weight: 600;
+        &:not(.active):hover{
+            color: orange;
+        }
     }
+    .tab.active {
+        box-shadow: inset 0 0 0 1px orange;
+        cursor: default;
+    }
+    .resume-sub-tab {
+        box-shadow: inset 0 0 0 1px orange;
+        font-weight: 400;
+        }
     .contents {
         border: 1px solid #ccc;
         height: fit-content;
@@ -112,20 +123,15 @@
         margin: 20px;
         padding: 20px;
     }
-    .contents::-webkit-scrollbar-track {
-        /* here in case you want to add stylings later */
-    }
     .contents::-webkit-scrollbar-thumb {
         background: orange;
     }
     .contents::-webkit-scrollbar {
         width: 1px;
     }
-
     .resume {
         width: 100%;
     }
-
     .resume-pop-up{
         position: fixed;
         top: 13vh;
