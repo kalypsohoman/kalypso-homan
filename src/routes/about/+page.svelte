ece4cf1acb197b213018578d67c88c783b57e5b9
kalypsohoman <kalypsohoman@gmail.com>
2024-03-26 12:30:07 -0500

    Remove todo, add some tab stylings

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,3 @@
-<!-- TODO: make bio only clickable from ghost :oooo -->
-
-
 <script lang=ts>
     import {Contact, Poem, PopupContainer, SocialMediaLinks} from '$lib';
 	import { quadInOut } from 'svelte/easing';
@@ -94,7 +91,6 @@
     .main {
         display: grid;
         grid-template-columns: 2fr 3fr;
-        gap: 20px;
         align-items: start;
         margin-top: 20px;
     }
@@ -103,57 +99,62 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        .headshot{
+           max-width: 80%;
+       }
     }
-     .headshot{
-        max-width: 80%;
-    }
-    .tab-container {
-        display: flex;
-        flex-wrap: wrap;
-        padding-left: 0;
-    }
-    .tab {
-        width: 20%;
-        height: 50px;
-        align-items: center;
-        cursor: pointer;
-        box-shadow: inset 0 0 0 1px white;
-        margin: 2%;
-        padding: 0;
-        display: flex;
-        justify-content: center;
-        background-color: transparent;
-        border: none;
-        color: white;
-        font-family: 'Caviar Dreams';
-        font-weight: 600;
-        &:not(.active):hover{
-            color: orange;
-        }
-    }
-    .tab.active {
-        box-shadow: inset 0 0 0 1px orange;
-        cursor: default;
-    }
-    .resume-sub-tab {
-        box-shadow: inset 0 0 0 1px orange;
-        font-weight: 400;
-        text-wrap: nowrap;
-        }
-    .contents {
-        border: 1px solid #ccc;
-        height: fit-content;
-        max-height: 63vh;
-        overflow-y: auto;
-        overflow-x: hidden;
+
+    .section-2 {
         margin: 20px;
-        padding: 20px;
-    }
-    .contents::-webkit-scrollbar-thumb {
-        background: orange;
-    }
-    .contents::-webkit-scrollbar {
-        width: 1px;
+        .tab-container {
+            display: flex;
+            flex-wrap: wrap;
+            padding-left: 0;
+            gap: 6.5%;
+            margin-bottom: 20px;
+        }
+        .tab {
+            width: 20%;
+            height: 50px;
+            align-items: center;
+            cursor: pointer;
+            box-shadow: inset 0 0 0 1px white;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            background-color: transparent;
+            border: none;
+            color: white;
+            font-family: 'Mourgen';
+            font-weight: 600;
+            font-size: min(1.2rem, 3.5vw);;
+            &:not(.active):hover{
+                color: orange;
+            }
+        }
+        .tab.active {
+            box-shadow: inset 0 0 0 1px orange;
+            cursor: default;
+        }
+        .resume-sub-tab {
+            box-shadow: inset 0 0 0 1px orange;
+            font-weight: 400;
+            text-wrap: nowrap;
+            font-size: min(1rem, 3.5vw);;
+            }
+        .contents {
+            border: 1px solid #ccc;
+            height: 70vh;
+            overflow-y: auto;
+            overflow-x: hidden;
+            padding: 20px;
+            &::-webkit-scrollbar-thumb {
+                background: orange;
+            }
+            &::-webkit-scrollbar {
+                width: 1px;
+            }
+        }
     }
     .resume {
         width: 100%;
