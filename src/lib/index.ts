fbbdb661b14f0e2ad77cc2dc12b83905f56f4c76
kalypsohoman <kalypsohoman@gmail.com>
2024-05-13 07:56:13 -0500

    Organize lib folder

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,8 @@
-export {default as Poem} from "./Poem.svelte";
-export {default as CloseButton} from "./CloseButton.svelte";
-export {default as Contact} from "./Contact.svelte";
-export {default as NavMenu} from "./NavMenu.svelte";
-export {default as PopupContainer} from "./PopupContainer.svelte";
-export {default as Project} from "./Project.svelte";
-export {default as SocialMediaLinks} from "./SocialMediaLinks.svelte";
-export { codeProjects } from "./codeProjects";
+export {default as Poem} from "./about-page/Poem.svelte";
+export {default as CloseButton} from "./shared/CloseButton.svelte";
+export {default as Contact} from "./about-page/Contact.svelte";
+export {default as NavMenu} from "./shared/NavMenu.svelte";
+export {default as PopupContainer} from "./shared/PopupContainer.svelte";
+export {default as Project} from "./home-page/Project.svelte";
+export {default as SocialMediaLinks} from "./about-page/SocialMediaLinks.svelte";
+export { codeProjects } from "./home-page/codeProjects";
