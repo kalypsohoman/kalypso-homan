23cc171feab40cb000306625bf83a25051c452c6
kalypsohoman <kalypsohoman@gmail.com>
2024-05-13 13:29:32 -0500

    Remove email export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,5 @@
 export {default as CloseButton} from "./shared/CloseButton.svelte";
 export {default as Contact} from "./about-page/Contact.svelte";
-export {default as Email} from "./about-page/emails/Email.svelte"
 export {default as NavMenu} from "./shared/NavMenu.svelte";
 export {default as Poem} from "./about-page/Poem.svelte";
 export {default as PopupContainer} from "./shared/PopupContainer.svelte";
