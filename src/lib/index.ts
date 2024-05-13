522e8083500546a57f6fa8d53f0514ff995f5a5d
kalypsohoman <kalypsohoman@gmail.com>
2024-05-13 12:33:56 -0500

    alphabetize

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
-export {default as Poem} from "./about-page/Poem.svelte";
 export {default as CloseButton} from "./shared/CloseButton.svelte";
 export {default as Contact} from "./about-page/Contact.svelte";
+export {default as Email} from "./about-page/emails/Email.svelte"
 export {default as NavMenu} from "./shared/NavMenu.svelte";
+export {default as Poem} from "./about-page/Poem.svelte";
 export {default as PopupContainer} from "./shared/PopupContainer.svelte";
 export {default as Project} from "./home-page/Project.svelte";
 export {default as SocialMediaLinks} from "./about-page/SocialMediaLinks.svelte";
